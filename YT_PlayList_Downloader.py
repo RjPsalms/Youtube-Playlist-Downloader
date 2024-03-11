a054a7e45dd5f8ef03c9f7a2da8b239a93cd1ff5
RjPsalms <157780202+RjPsalms@users.noreply.github.com>
2024-03-11 14:02:49 +0400

    Update YT_PlayList_Downloader.py

--- YT_PlayList_Downloader.py
+++ YT_PlayList_Downloader.py
@@ -23,15 +23,15 @@ class YouTubeDownloaderApp(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("YT Playlist Downloader")
-        self.geometry("640x400")
+        self.geometry("600x400")
         self.grid_columnconfigure((0,1,2), weight=1)
         self.grid_rowconfigure(4, weight=1)
 
         # Create an entry field for the playlist link
-        self.link_entry = ctk.CTkEntry(self, placeholder_text="    ... < Youtube link > ...")
+        self.link_entry = ctk.CTkEntry(self, placeholder_text="    ...... <  Youtube links only  > ......")
         self.link_entry.grid(row=0, column=1, padx=10, pady=15, sticky="ew", columnspan=2)
         
-        self.input_label = ctk.CTkLabel(self, text="Paste Youtube Link here  >>>", font=("",15))
+        self.input_label = ctk.CTkLabel(self, text="Paste link here  >>>", font=("",14))
         self.input_label.grid(row=0, column=0, sticky="ew", columnspan=1)
         
         # Create label to display thumbnail
@@ -39,19 +39,19 @@ class YouTubeDownloaderApp(ctk.CTk):
         self.center_image.grid(row=1, column=1, rowspan=2, sticky="nsew")
 
         # Create a download button for playlist download video
-        self.download_button_video = ctk.CTkButton(self, height=35, text="Download Playlist (Video)", font=("", 15),command=self.download_playlist_video)
+        self.download_button_video = ctk.CTkButton(self, height=35, text="Download Playlist (Video)", font=("", 15),fg_color="#1985a1",command=self.download_playlist_video)
         self.download_button_video.grid(row=1, column=0, padx=15, pady=5, sticky="ew")
 
         # Create a download button for audio
-        self.download_button_audio = ctk.CTkButton(self, height=35, text="Download Playlist (Audio)", font=("", 15), command=self.download_playlist_audio)
+        self.download_button_audio = ctk.CTkButton(self, height=35, text="Download Playlist (Audio)", font=("", 15),fg_color="#1985a1", command=self.download_playlist_audio)
         self.download_button_audio.grid(row=2, column=0, padx=15, pady=5, sticky="ew")
         
         # Create a button for single video download
-        self.download_button_single_video = ctk.CTkButton(self, height=35, text="Download Video", font=("", 15), command=self.download_single_video)
+        self.download_button_single_video = ctk.CTkButton(self, height=35, text="Download Video", font=("", 15),fg_color="#1985a1", command=self.download_single_video)
         self.download_button_single_video.grid(row=1, column=2, padx=15, pady=5, sticky="ew")
 
         # Create a button for single audio download
-        self.download_button_single_audio = ctk.CTkButton(self, height=35, text="Download Audio", font=("", 15), command=self.download_single_audio)
+        self.download_button_single_audio = ctk.CTkButton(self, height=35, text="Download Audio", font=("", 15),fg_color="#1985a1", command=self.download_single_audio)
         self.download_button_single_audio.grid(row=2, column=2, padx=15, pady=5, sticky="ew")
   
         # Create a progress bar
@@ -67,15 +67,15 @@ class YouTubeDownloaderApp(ctk.CTk):
         self.text_box.grid(row=4, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
         
         # Create a button to clear the text box
-        self.clear_text_button = ctk.CTkButton(self, text="Clear Text Box", command=self.clear_text_box)
+        self.clear_text_button = ctk.CTkButton(self, text="Clear Text Box",fg_color="#1985a1",corner_radius=50, command=self.clear_text_box)
         self.clear_text_button.grid(row=6, column=1, padx=20, pady=10, sticky="ew")
         
         # Create a browse button for selecting the download path
-        self.browse_button = ctk.CTkButton(self ,text="Download path", command=self.browse_path)
+        self.browse_button = ctk.CTkButton(self ,text="Download path",fg_color="#1985a1",corner_radius=50, command=self.browse_path)
         self.browse_button.grid(row=6, column=0, padx=20, pady=10, sticky="ew")
 
         # Create a cancel button
-        self.cancel_button = ctk.CTkButton(self, text="CANCEL", font=("",15),text_color="black",fg_color="gray", hover_color="red", command=self.cancel_download, state="disabled")
+        self.cancel_button = ctk.CTkButton(self, text="CANCEL", font=("",15),text_color="black",corner_radius=50,fg_color="gray", hover_color="red", command=self.cancel_download, state="disabled")
         self.cancel_button.grid(row=6, column=2, padx=20, pady=10, sticky="ew")
 
         # Initialize the download status, cancel flag, and download path
@@ -92,17 +92,27 @@ class YouTubeDownloaderApp(ctk.CTk):
     def cancel_download(self):
         if self.is_downloading:
             self.cancel_flag = True
-            self.text_box.insert("end", "\n... Cancelling all downloads ...\n")
+            self.text_box.insert("end", "\n ... Cancelling all downloads ...\n")
             self.text_box.see("end")
             # Disable the cancel button during the cancellation
             self.cancel_button.configure(state="disabled")
         else:
-            self.text_box.insert("end", "\nError: No download in progress.\n")
+            self.text_box.insert("end", "\n Error: No download in progress.\n")
             self.text_box.see("end")
+
+    def clear_text_box(self):
+        # Clear the contents of the text box
+        self.text_box.delete("1.0", "end")
+        self.link_entry.delete(0, "end")
+        self.center_image.configure(image=None)
+        app.download_label.configure(text="0%")
+        app.progress_bar.set(0)
+        
+        self.cancel_flag = False
 
     def download_single_video(self):
         if not self.link_entry.get():
-            self.text_box.insert("end", "\nEnter a video link.\n")
+            self.text_box.insert("end", "\n Enter a video link.\n")
             self.text_box.see("end")
             return
 
@@ -110,7 +120,7 @@ class YouTubeDownloaderApp(ctk.CTk):
 
     def download_single_audio(self):
         if not self.link_entry.get():
-            self.text_box.insert("end", "\nEnter an audio link.\n")
+            self.text_box.insert("end", "\n Enter an audio link.\n")
             self.text_box.see("end")
             return
 
@@ -131,7 +141,7 @@ class YouTubeDownloaderApp(ctk.CTk):
         try:
             yt_video = YouTube(p_link)
         except Exception as e:
-            self.text_box.insert("end", f"\nError: {str(e)}\n")
+            self.text_box.insert("end", f"\nError: Please make sure that the link is a valid Youtube video link and not Playlist.\n")
             self.text_box.see("end")
             self.is_downloading = False  # Reset download status
             return
@@ -187,7 +197,7 @@ class YouTubeDownloaderApp(ctk.CTk):
     # The following method is for Playlist download
     def download_playlist_video(self):
         if not self.link_entry.get():
-            self.text_box.insert("end", "\nEnter a Playlist link.\n")
+            self.text_box.insert("end", "\n Enter a Playlist link.\n")
             self.text_box.see("end")
             return
         
@@ -195,24 +205,14 @@ class YouTubeDownloaderApp(ctk.CTk):
 
     def download_playlist_audio(self):
         if not self.link_entry.get():
-            self.text_box.insert("end", "\nEnter a Playlist link.\n")
+            self.text_box.insert("end", "\n Enter a Playlist link.\n")
             self.text_box.see("end")
             return
         
         threading.Thread(target=self.download_playlist, args=("audio",)).start()
 
-    def clear_text_box(self):
-        # Clear the contents of the text box
-        self.text_box.delete("1.0", "end")
-        self.link_entry.delete(0, "end")
-        self.center_image.configure(image=None)
-        app.download_label.configure(text="0%")
-        app.progress_bar.set(0)
-        
-        self.cancel_flag = False
-
     def download_playlist(self, mode="video"):
-        app.download_label.configure(text=f"Downloading ({mode.capitalize()})")
+        app.download_label.configure(text=f" Downloading ({mode.capitalize()})")
         
         self.is_downloading = True  # Set download status to True
         self.cancel_flag = False  # Reset cancel flag
@@ -222,19 +222,19 @@ class YouTubeDownloaderApp(ctk.CTk):
         try:
             p_link = self.link_entry.get()
         except Exception as e:
-            self.text_box.insert("end", "Please enter valid Playlist link!")
+            self.text_box.insert("end", " Please enter valid Playlist link!")
             
         try:
             link = Playlist(p_link)
         except Exception as e:
-            self.text_box.insert("end", f"\nError: {str(e)}\n")
+            self.text_box.insert("end", f"\n Error: Make sure that the link entered is a Playlist link.\n")
             self.text_box.see("end")
             self.is_downloading = False  # Reset download status
             return
         try:
             num_videos = len(link.video_urls)
         except Exception as e:
-            self.text_box.insert("end", "Please enter valid Playlist link!\n")
+            self.text_box.insert("end", " Please enter valid Playlist link!\n")
 
         for idx, video_url in enumerate(link.video_urls, start=1):
             if self.cancel_flag:
@@ -244,7 +244,7 @@ class YouTubeDownloaderApp(ctk.CTk):
             try:
                 yt_video = YouTube(video_url)
             except Exception as e:
-                self.text_box.insert("end", f"\nError fetching video: {str(e)}\n")
+                self.text_box.insert("end", f"\n Error fetching video: {str(e)}\n")
                 self.text_box.see("end")
                 continue
             
@@ -253,7 +253,7 @@ class YouTubeDownloaderApp(ctk.CTk):
             thumbnail_image = self.load_thumbnail(thumbnail_url)
             self.center_image.configure(image=thumbnail_image)
 
-            self.text_box.insert("end", f"Files to download: {num_videos}\n")
+            self.text_box.insert("end", f" Files to download: {num_videos}\n")
             app.progress_bar.set(0)
             yt_video.register_on_progress_callback(on_progress)
 
@@ -263,9 +263,9 @@ class YouTubeDownloaderApp(ctk.CTk):
                 elif mode == "audio":
                     stream = yt_video.streams.get_audio_only()
                 else:
-                    raise ValueError("Invalid mode. Use 'video' or 'audio'.")
+                    raise ValueError(" Invalid mode. Use 'video' or 'audio'.")
             except Exception as e:
-                self.text_box.insert("end", f"\nError getting stream: {str(e)}\n")
+                self.text_box.insert("end", f"\n Error getting stream: {str(e)}\n")
                 self.text_box.see("end")
                 continue
 
@@ -275,7 +275,7 @@ class YouTubeDownloaderApp(ctk.CTk):
             try:
                 stream.download(download_path)
             except Exception as e:
-                self.text_box.insert("end", f"\nError downloading: {str(e)}\n")
+                self.text_box.insert("end", f"\n Error downloading: {str(e)}\n")
                 self.text_box.see("end")
                 continue
             
@@ -291,7 +291,6 @@ class YouTubeDownloaderApp(ctk.CTk):
         # Reset the entry box to an empty state after all downloads are completed
         self.link_entry.delete(0, "end")
         self.cancel_button.configure(state="disabled")
-        
 
 if __name__ == "__main__":
     app = YouTubeDownloaderApp()
